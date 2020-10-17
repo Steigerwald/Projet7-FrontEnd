5ec53954cc1aacca9a1d51867aa592b0ea00138e
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2020-10-17 11:03:42 +0200

    premier jet de mise en place sur Bibliotheques

--- src/main/java/Project7/FrontEnd/controller/BibliothequeController.java
+++ src/main/java/Project7/FrontEnd/controller/BibliothequeController.java
@@ -1,21 +1,34 @@
 package Project7.FrontEnd.controller;
 
 import Project7.FrontEnd.dto.BibliothequeDTO;
+import Project7.FrontEnd.service.BibliothequeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
+@Controller
 @RequestMapping("/bibliotheque")
 public class BibliothequeController {
 
+    @Autowired
+    public BibliothequeService bibliothequeService;
+
+
     /* controller pour avoir toutes les bibliotheques*/
-    @RequestMapping(path="/",method = RequestMethod.GET)
-    public String getAllBibliotheques(Model model) {
-        List<BibliothequeDTO> toutesBibliotheques= bibliothequeService.findAll();
-        model.addAttribute("bibliotheques",toutesBibliotheques);
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String getAllBibliotheques(Model model) throws IOException {
+        //Recupérer les données json via la méthode getAllNotes(), les convertir en objets java et les stocker dans une liste
+        List<BibliothequeDTO> bibliotheques = bibliothequeService.getAllBibliotheques();
+        //Liste stockée à son tour dans un ModelMap renvoyée dans la réponse.
+        ModelMap mp = new ModelMap("bibliotheques", bibliotheques);
+        model.addAttribute("bibliotheques",mp);
         return "ListeBibliotheques";
     }
 
--- src/main/java/Project7/FrontEnd/service/BibliothequeService.java
+++ src/main/java/Project7/FrontEnd/service/BibliothequeService.java
@@ -1,26 +1,34 @@
 package Project7.FrontEnd.service;
 
 import Project7.FrontEnd.dto.BibliothequeDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@Service
 public class BibliothequeService {
 
     Logger logger = (Logger) LoggerFactory.getLogger(BibliothequeService.class);
 
     /*Methode pour obtenir tous les sites de la base de données*/
-    public List<BibliothequeDTO> getAllBibliotheques() {
-        List<BibliothequeDTO> result1 =(List<BibliothequeDTO>) siteRepository.findAll();
-        if(result1.size() > 0) {
-            logger.info(" retour liste result1 si taille de result1 >0 ");
-            return result1;
+    public List<BibliothequeDTO> getAllBibliotheques() throws IOException {
+
+        ObjectMapper mapper =new ObjectMapper();
+        List<BibliothequeDTO> toutesBibliotheques = mapper.readValue(new URL("http://localhost:5500/my-notes"), List.class);
+        if(toutesBibliotheques.size() > 0) {
+            logger.info(" retour liste toutesBibliotheques car la taille de laliste >0 ");
+            return toutesBibliotheques;
         } else {
-            logger.info(" retour nouvelle liste car pas d'élément dans la liste result1 ");
+            logger.info(" retour d'une nouvelle liste car pas d'élément dans la liste toutesBibliotheques ");
             return new ArrayList<BibliothequeDTO>();
         }
     }
-
 }
