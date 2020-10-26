9b612a1c531faa56847c9511eb39074c00922025
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2020-10-27 00:47:16 +0100

    mise en place des formats de date dans livreDTO de frontEnd

--- src/main/java/Project7/FrontEnd/dto/LivreDTO.java
+++ src/main/java/Project7/FrontEnd/dto/LivreDTO.java
@@ -15,14 +15,37 @@ public class LivreDTO {
     private int idLivre;
     private String titre;
     private String auteur;
-    private String publication;
+    private Date publication;
     private String resume;
     private String nombrePages;
     private String nomCategorie;
-    private String dateAchat;
+    private Date dateAchat;
     private int prixLocation;
     private String etatLivre;
     private Boolean disponibilite;
     private ReservationDTO reservation;
+
+    public String getPublication() {
+        SimpleDateFormat dateFormat
+                = new SimpleDateFormat("yyyy-MM");
+        String dateString = dateFormat.format(publication);
+        return dateString;
+    }
+
+    public String getDateAchat() {
+        SimpleDateFormat dateFormat
+                = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String dateString = dateFormat.format(dateAchat);
+        return dateString;
+    }
+
+    public String getDisponibilite() {
+        if (disponibilite){
+            return "oui";
+        } else{
+            return "non";
+        }
+    }
+
 
 }
