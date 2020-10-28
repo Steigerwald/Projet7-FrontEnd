aa5c3857695a09cb8449ac46e739680580ec2ef5
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2020-10-28 01:19:46 +0100

    corrections mineures

--- src/main/java/Project7/FrontEnd/service/BibliothequeService.java
+++ src/main/java/Project7/FrontEnd/service/BibliothequeService.java
@@ -2,17 +2,13 @@ package Project7.FrontEnd.service;
 
 import Project7.FrontEnd.dto.BibliothequeDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.NameTokenizers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
