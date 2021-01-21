adb1a8903aedb27537c808d76bd73b848e4d4237
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2021-01-21 19:09:01 +0100

    version pour joffrey

--- src/main/java/Project7/FrontEnd/dto/LivreDTO.java
+++ src/main/java/Project7/FrontEnd/dto/LivreDTO.java
@@ -2,7 +2,6 @@ package Project7.FrontEnd.dto;
 
 import lombok.Data;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -21,26 +20,6 @@ public class LivreDTO {
     private String etatLivre;
     private Boolean disponibilite;
     private BibliothequeDTO bibliotheque;
-
-    /*public String getPublication() {
-
-        SimpleDateFormat simpleDateFormat01 = new SimpleDateFormat("MM-yyyy");
-        return simpleDateFormat01.format(publication);
-    }
-
-    public String getDateAchat() {
-        SimpleDateFormat simpleDateFormat02 = new SimpleDateFormat("dd-MM-yyyy");
-        return simpleDateFormat02.format(dateAchat);
-    }
-    public LivreDTO() throws ParseException {
-        SimpleDateFormat simpleDateFormat01 = new SimpleDateFormat("MM-yyyy-dd HH:mm:ss");
-        String date1 = "2005-04-28 02:45:30";
-        publication=simpleDateFormat01.parse(date1);
-        SimpleDateFormat simpleDateFormat02 = new SimpleDateFormat("MM-yyyy-dd HH:mm:ss");
-        String date2 = "2005-04-28 02:45:30";
-        dateAchat=simpleDateFormat02.parse(date2);
-        setDisponibilite(true);
-    }*/
 
     public String toStringDatePublication() {
 
